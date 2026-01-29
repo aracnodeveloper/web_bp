ce8b70df97fe6ea00f3cb6e92d66a1b5a1cfd563
AracnoGrpup <146010233+anthonyramosc@users.noreply.github.com>
2026-01-29 11:06:10 -0500

    rute

--- src/constants/EndpointsRoutes.js
+++ src/constants/EndpointsRoutes.js
@@ -1,32 +1,32 @@
 //AUTH
-export const loginApi = "/bp_api/auth/login"
-export const registerStudentApi = "/bp_api/auth/register"
+export const loginApi = "/auth/login"
+export const registerStudentApi = "/auth/register"
 
 //ABOUTME
-export const aboutmeApi = "/bp_api/aboutme"
+export const aboutmeApi = "/aboutme"
 
 //METRICS
-export const socialsApi = "/bp_api/socials"
-export const genderApi = "/bp_api/gender"
-export const ageApi = "/bp_api/age"
-export const locationApi = "/bp_api/location"
+export const socialsApi = "/socials"
+export const genderApi = "/gender"
+export const ageApi = "/age"
+export const locationApi = "/location"
 
 //CONTENT (REDES SOCIALES)
-export const contentApi = "/bp_api/content"
+export const contentApi = "/content"
 
 //GALLERY (ENTREVISTAS)
-export const galleryApi = "/bp_api/gallery"
+export const galleryApi = "/gallery"
 
 //INFLUENCERS (PROYECTOS)
-export const influencersApi = "/bp_api/influencers"
-export const socialLinksApi = "/bp_api/social"
-export const videosApi = "/bp_api/videos"
+export const influencersApi = "/influencers"
+export const socialLinksApi = "/social"
+export const videosApi = "/videos"
 
 //TEXT
-export const textApi = "/bp_api/text"
+export const textApi = "/text"
 
 //UPLOADS
-export const uploadImageApi = "/bp_api/upload/image"
-export const uploadImageInfluencerApi = "/bp_api/upload/image-influencer"
-export const uploadImageVideoApi = "/bp_api/upload/image-video"
-export const deleteImageApi = "/bp_api/upload/image"
+export const uploadImageApi = "/upload/image"
+export const uploadImageInfluencerApi = "/upload/image-influencer"
+export const uploadImageVideoApi = "/upload/image-video"
+export const deleteImageApi = "/upload/image"
