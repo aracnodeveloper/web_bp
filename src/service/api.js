8a69736bc9401d78bf673a19f03ce2a3cbac5a27
AracnoGrpup <146010233+anthonyramosc@users.noreply.github.com>
2026-01-29 10:24:22 -0500

    *

--- src/service/api.js
+++ src/service/api.js
@@ -10,6 +10,7 @@ const api = axios.create({
     headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
     },
 });
 
